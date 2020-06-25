18efdc78ad52eb48d95c217073e1740458b67239
siriiana <44429002+siriiana@users.noreply.github.com>
2020-06-25 17:03:14 +0300

    Added css and js
    Got the functionality right, final tweaks needed for style.cs

---
+++ Survey_Form/javascript.js
@@ -0,0 +1,31 @@
+var formSubmit = {name: "", email: "", age: "", mood: "", recommend: "", improved: [], comments: ""}
+
+
+
+function printForm(event) {
+    event.preventDefault();
+
+    if (!null) {
+    formSubmit.name = document.getElementById('name').value;
+    
+    formSubmit.email = document.getElementById('email').value;
+    
+    formSubmit.age = document.getElementById('age').value;
+
+    formSubmit.mood = document.querySelector('.mood option:checked').value;
+    
+    formSubmit.recommend = document.querySelector('input[name="recommend"]:checked').value;
+
+    var improvements = document.querySelectorAll('input[type=checkbox]:checked')
+    for (var i = 0; i < improvements.length; i++) {
+      formSubmit.improved.push(improvements[i].value)
+    };
+
+    formSubmit.comments = document.getElementById('comments').value;
+
+    console.log(formSubmit);
+}
+if(null) {
+    alert("Please fill otu the form before submitting");
+}
+};
