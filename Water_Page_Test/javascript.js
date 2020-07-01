c0b7069bde23d1056912519ddd637697c42291e5
siriiana <44429002+siriiana@users.noreply.github.com>
2020-07-01 12:41:12 +0300

    Trying to fix play problem
    Still doesn't work

--- Water_Page_Test/javascript.js
+++ Water_Page_Test/javascript.js
@@ -2,7 +2,4 @@ function animation() {
     document.getElementById("video").style.animationPlayState = "running";
     document.getElementById("button").style.display = "none";
     document.getElementById("main").style.animationPlayState = "running";
-    if (document.getElementById("video").style.animationPlayState != "running") {
-        document.getElementById("main").style.visibility ="visible";
-    }
 }
